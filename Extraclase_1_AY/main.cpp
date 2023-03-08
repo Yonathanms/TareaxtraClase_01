c949791630d44a0dc485643da6ec66419c70baf3
Axel_Flores <axeel@estudiantec.cr>
2023-03-08 12:32:06 -0600

    Se implementan las primeras funcionalidades
    Se crean los primeros punteros y la funcionalidad inicial de la lista enlazada simple a partir de "Nodos", propensas estas definiciones a futuros cambios.

--- Extraclase_1_AY/main.cpp
+++ Extraclase_1_AY/main.cpp
@@ -2,19 +2,48 @@
 #include <iostream>
 #include <cstddef>
 #include <list>
-using namespace std;
 
-//clase Node
+//Definir clase Node
 class Node{
-
+    //objeto valor, que almacena el valor del nodo
+    int valor;
+    
+    //puntero a otro objeto de tipo Node (almacena la direccion de memoria del siguiente nodo en la lista)
+    Node *siguiente;
 }
 
-//clase List
+//Definir clase List(para implementar una lista enlazada simple)
 class List{
 
+    //definir puntero principal para apuntar al primer nodo de la lista(Objeto de tipo Node)
+    Node *principal;
+
+    //constructor de la clase List
+    //inicializa el puntero en un valor "null", que significa que la lista no cuenta con valores en un inicio.
+    List() : principal(nullptr){}
+
+    //Declarar el metodo de insercion para la clase List
+    //Toma un "valor" definido anteriormente de tipo entero
+    //tipo void porque no devuelve nada
+    void insertar(int valor)
 }
 
-//clase Collector
+//definicion del metodo insertar declarado en la clase llamada List{}
+//el metodo toma un "valor" entero como parametro e inserta un nuevo objeto Node al inicio
+void List::insertar(int valor){
+
+    //Asignar memoria para un nuevo "Node" usando malloc, el "Node" especifica el tamaño de memoria a asignar
+    Node *nodo = (Node*)malloc(sizeof(Node));
+
+    nodo->valor = valor;
+    nodo->siguiente = principal;
+    head = nodo;
+
+
+}
+
+
+//Definir clase Collector
 class Collector{
 
 }
